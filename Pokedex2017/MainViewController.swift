36100185dd3d423ee981a4a85bffd2de7323770e
Patrick Pahl <patrickpahl@Patricks-MacBook-Pro.local>
2017-04-30 17:04:59 -0600

    Completed

--- Pokedex2017/MainViewController.swift
+++ Pokedex2017/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MainViewController: UIViewController, UISearchBarDelegate {
     // *Added searchbar delegate
-
+    
     
     @IBOutlet var nameLabel: UILabel?
     @IBOutlet var moveLabel: UILabel?
@@ -18,17 +18,48 @@ class MainViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet var pokemonImageView: UIImageView?
     @IBOutlet var searchBar: UISearchBar?
     
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar?.delegate = self
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else { return }
+        PokemonController.fetchPokemon(for: searchTerm) { (pokemon) in
+            guard let pokemon = pokemon else { return }
+            DispatchQueue.main.async {
+                self.updateWith(pokemon: pokemon)
+            }
+        }
+    }
+    
+    func updateWith(pokemon: Pokemon) {
+        ImageController.image(forURL: pokemon.pictureURL, completion: { (image) in
+            self.pokemonImageView?.image = image
+        })
+        
+        let formattedString = NSMutableAttributedString()
+        nameLabel?.attributedText = formattedString.normal(text: "Name: ").bold(text: pokemon.name.capitalized)
+        moveLabel?.text = "Move: " + pokemon.moveName.capitalized
+        typeLabel?.text = "Type: " + pokemon.type.capitalized
+    }
+}
 
+extension NSMutableAttributedString {
+    func bold(text:String) -> NSMutableAttributedString {
+        let attrs:[String:AnyObject] = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 17.0) ]
+        let boldString = NSMutableAttributedString(string:"\(text)", attributes:attrs)
+        self.append(boldString)
+        return self
+    }
+    
+    func normal(text:String)->NSMutableAttributedString {
+        let normal =  NSAttributedString(string: text)
+        self.append(normal)
+        return self
     }
 
-
-
+    
     
     
     
