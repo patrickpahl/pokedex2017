7b72964ee554d6a951784e83ba6cd14644e2b6ed
Patrick Pahl <patrickpahl@Patricks-MacBook-Pro.local>
2017-04-30 16:06:51 -0600

    InitialCommit

---
+++ Pokedex2017/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Pokedex2017
+//
+//  Created by Patrick Pahl on 4/30/17.
+//  Copyright © 2017 Patrick Pahl. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+    
+    
+    
+    
+}
---
+++ Pokedex2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex2017
+//
+//  Created by Patrick Pahl on 4/30/17.
+//  Copyright © 2017 Patrick Pahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
